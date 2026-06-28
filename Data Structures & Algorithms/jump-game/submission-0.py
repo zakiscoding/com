aa4a92db9673662bba84228476c21264691a65a8
Zakariye Mohamed <168684030+zakiscoding@users.noreply.github.com>
2026-06-27 22:53:20 -0500

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        furthest=0
+        for i in range(len(nums)):
+            if i > furthest:
+                return False
+            furthest=max(furthest, i+nums[i])
+            
+        return True
