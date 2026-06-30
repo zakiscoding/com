b5a1105994c0fa56aaafc9e83305fd354cea8674
Zakariye Mohamed <168684030+zakiscoding@users.noreply.github.com>
2026-06-30 00:45:14 -0500

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,25 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store= {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key]= []
+        self.store[key].append([timestamp,value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ''
+        values = self.store[key]
+        left = 0
+        right= len(values)-1
+        res = ''
+        while left<=right:
+            mid = (right+left)//2
+            if values[mid][0]<=timestamp:
+                res = values[mid][1]
+                left= mid +1
+            else:
+                right= mid -1
+        return res
