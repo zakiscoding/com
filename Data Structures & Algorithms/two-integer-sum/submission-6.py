385bf530b1f16cfe31b001eab3dfb097347c1dfb
Zakariye Mohamed <168684030+zakiscoding@users.noreply.github.com>
2026-07-01 13:26:16 -0500

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash1 = {}
+        for i,v in enumerate(nums):
+            if target-v in hash1:
+                return [hash1[target-v], i]
+            hash1[v]=i
